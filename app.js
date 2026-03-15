f4db95f9ef2d254d7f322798f39a437bbb0cea0a
Vishwajeet <nadargevishwajeet@gmail.com>
2026-03-15 10:47:54 +0530

    add edit route

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
+const methodOverride = require("method-override");
 
 main()
     .then((req, res) => {
@@ -17,6 +18,7 @@ async function main() {
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 app.get("/", (req, res) => {
     res.send("hi i am groot");
@@ -45,6 +47,20 @@ app.get("/listings/:id", async (req, res) => {
     const listing = await Listing.findById(id);
     res.render("listings/show.ejs", { listing });
 })
+
+// edit
+app.get("/listings/:id/edit", async (req, res) => {
+    let { id } = req.params;
+    let listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", { listing });
+})
+
+app.put("/listings/:id", async (req, res) => {
+    let { id } = req.params;
+    let newListing = req.body.listing;
+    await Listing.findByIdAndUpdate(id, newListing);
+    res.redirect(`/listings/${id}`);
+});
 
 app.listen(8080, () => {
     console.log("app is listening on port 8080");
