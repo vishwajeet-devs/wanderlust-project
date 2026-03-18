b1af67adc5f89457dc225b4d8c33c0ad906577b6
Vishwajeet <nadargevishwajeet@gmail.com>
2026-03-18 17:32:44 +0530

    add server side validation and custom error handling

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
 const engine = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
 
 main()
     .then((req, res) => {
@@ -28,48 +30,64 @@ app.get("/", (req, res) => {
 })
 
 // Index
-app.get("/listings", async (req, res) => {
+app.get("/listings", wrapAsync(async (req, res, next) => {
     let allListings = await Listing.find({});
     res.render("listings/index.ejs", { allListings });
-});
+}));
 
 // New
 app.get("/listings/new", (req, res) => {
     res.render("listings/new.ejs");
 })
 
-app.post("/listings", async (req, res) => {
+app.post("/listings", wrapAsync(async (req, res, next) => {
+    if(!req.body.listing){
+        throw new ExpressError(400, "send valid data for listing");
+    }
     let  listing = req.body.listing;
     await Listing.create(listing);
     res.redirect("/listings");
-})
+}));
 
 // show 
-app.get("/listings/:id", async (req, res) => {
+app.get("/listings/:id", wrapAsync(async (req, res, next) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/show.ejs", { listing });
-})
+}));
 
 // edit
-app.get("/listings/:id/edit", async (req, res) => {
+app.get("/listings/:id/edit", wrapAsync(async (req, res) => {
     let { id } = req.params;
     let listing = await Listing.findById(id);
     res.render("listings/edit.ejs", { listing });
-})
+}));
 
-app.put("/listings/:id", async (req, res) => {
+app.put("/listings/:id", wrapAsync(async (req, res) => {
+    if(!req.body.listing){
+        throw new ExpressError(400, "send valid data for listing");
+    }
     let { id } = req.params;
     let newListing = req.body.listing;
     await Listing.findByIdAndUpdate(id, newListing);
     res.redirect(`/listings/${id}`);
-});
+}));
 
 // Delete
-app.delete("/listings/:id", async (req, res) => {
+app.delete("/listings/:id", wrapAsync(async (req, res) => {
     let { id } = req.params;
     await Listing.findByIdAndDelete(id);
     res.redirect("/listings");
+}));
+
+app.use((req, res, next) => {
+    next(new ExpressError(404, "page not found"));
+});
+
+// middleware - error handler
+app.use((err, req, res, next) => {
+    let { statusCode=500, message="Something went wrong" } = err;
+    res.status(statusCode).send(message);
 })
 
 app.listen(8080, () => {
