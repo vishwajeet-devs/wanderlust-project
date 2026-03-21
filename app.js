bc115deac8251d7051664541ed064d4085e0c5e3
Vishwajeet <nadargevishwajeet@gmail.com>
2026-03-22 00:00:58 +0530

    feat: add review deletion

--- app.js
+++ app.js
@@ -111,6 +111,14 @@ app.post("/listings/:id/reviews", validateReview, wrapAsync(async (req, res) => 
     res.redirect(`/listings/${listing._id}`);
 }));
 
+// Delete review
+app.delete("/listings/:id/reviews/:reviewId", wrapAsync(async(req, res) => {
+    let { id, reviewId } = req.params;
+    await Listing.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/listings/${id}`);
+}));
+
 app.use((req, res, next) => {
     next(new ExpressError(404, "page not found"));
 });
--- models/listing.js
+++ models/listing.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Review = require("./review.js");
 
 const listeningSchema = new Schema({
     title: {
@@ -21,6 +22,13 @@ const listeningSchema = new Schema({
             ref: "Review",
         }
     ]
+});
+
+// middleware
+listeningSchema.post("findOneAndDelete", async (listing) => {
+    if(listing){
+        await Review.deleteMany({_id: {$in: listing.reviews}});
+    }
 })
 
 const Listing = mongoose.model("Listing", listeningSchema);
