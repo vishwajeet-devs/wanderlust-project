f172a5948e9852c17ed0cdb83ba2f7e4f043b67b
Vishwajeet <nadargevishwajeet@gmail.com>
2026-03-21 16:16:42 +0530

    add client-side and server-side validation for reviews

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const methodOverride = require("method-override");
 const engine = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-const { listingSchema } = require("./schema.js");
+const { listingSchema, reviewSchema } = require("./schema.js");
 const Review = require("./models/review.js");
 
 main()
@@ -34,6 +34,15 @@ app.get("/", (req, res) => {
 // mddleware function for schema validations
 const validateListing = (req, res, next) => {
     let { error } = listingSchema.validate(req.body);
+    if(error){
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(404, errMsg);
+    } else next();
+}
+
+// validate review
+const validateReview = (req, res, next) => {
+    let { error } = reviewSchema.validate(req.body);
     if(error){
         let errMsg = error.details.map((el) => el.message).join(",");
         throw new ExpressError(404, errMsg);
@@ -90,7 +99,7 @@ app.delete("/listings/:id", wrapAsync(async (req, res) => {
 
 // reviews
 // post route
-app.post("/listings/:id/reviews", async (req, res) => {
+app.post("/listings/:id/reviews", validateReview, wrapAsync(async (req, res) => {
     let listing = await Listing.findById(req.params.id);
     let newReview = new Review(req.body.review);
 
@@ -100,7 +109,7 @@ app.post("/listings/:id/reviews", async (req, res) => {
     await listing.save();
 
     res.redirect(`/listings/${listing._id}`);
-})
+}));
 
 app.use((req, res, next) => {
     next(new ExpressError(404, "page not found"));
