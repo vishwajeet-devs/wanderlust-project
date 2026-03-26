0016c88c33a6d038389c6780bf21767573d506ca
Vishwajeet <nadargevishwajeet@gmail.com>
2026-03-26 16:50:52 +0530

    feat: add user signup with Passport.js authentication and session support

--- app.js
+++ app.js
@@ -6,10 +6,14 @@ const methodOverride = require("method-override");
 const engine = require("ejs-mate");
 const session = require("express-session");
 const ExpressError = require("./utils/ExpressError.js");
-const flash = require("connect-flash")
+const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
 
-const listings = require("./routes/listing.js");
-const reviews = require("./routes/review.js");
+const listingRouter = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
 
 main()
     .then((req, res) => {
@@ -46,14 +50,32 @@ app.get("/", (req, res) => {
 app.use(session(sessionOptions));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next) => {
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
     next();
+});
+
+app.get("/demouser", async (req, res) => {
+    let fakeUser = {
+        email: "student@gmail.com",
+        username: "delta-student",
+    }
+
+    let registeredUser = await User.register(fakeUser, "helloworld");
+    res.send(registeredUser);
 })
 
-app.use("/listings", listings);
-app.use("/listings/:id/reviews", reviews);
+app.use("/listings", listingRouter);
+app.use("/listings/:id/reviews", reviewRouter);
+app.use("/", userRouter);
 
 app.use((req, res, next) => {
     next(new ExpressError(404, "page not found"));
